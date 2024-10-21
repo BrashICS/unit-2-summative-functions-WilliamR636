0c05d371f9697b27c9debb32df2ca5a7de712927
William Rodgers <william.rodgers@stu.ocsb.ca>
2024-10-21 11:23:01 -0400

    Day 1

--- main.js
+++ main.js
@@ -11,7 +11,7 @@
 /*** Event Listeners ***/
 document.getElementById("zeros").addEventListener("click", zeros);
 document.getElementById("vertex").addEventListener("click", vertex);
-
+ 
 /*** Functions ***/
 
 // Round to the nearest `decimals` number of decimals
@@ -23,6 +23,8 @@ function round(value, decimals) {
 function round_user(value) {
     // Get the number of decimals from the "rounding" box
     let d = Number(document.getElementById("rounding").value)
+
+    return round(value, d)
 
 }
 
@@ -39,4 +41,66 @@ function zeros() {
 function vertex() {
 
 }
+
+////// FIRST PART
+
+// Part 1
+
+function delta(a, b) {
+
+return a - b
+
+}
+
+// Part 2
+
+function slope(a1, a2, b1, b2) {
+
+return delta(b1, b2) / delta(a1, a2)
+
+}
+
+// Part 3
+
+function average(a1, a2) {
+
+return (a1 + a2) / 2
+
+}
+
+// Part 4 #### Already Made
+
+
+
+// Part 5
+
+function length (x1, y1, x2, y2) {
+
+return Math.sqrt(delta(x1, x2)**2 + delta(y1, y2)**2)
+
+}
+
+// Part 6 ### Already Made
+
+// Part 7
+
+// Part 8
+
+// Part 9
+
+// Part 10
+
+// Part 11
+
+
+
+////// SECOND PART
+
+// Part 1
+
+
+
+////// LAST PART
+
+// Part 1
 
